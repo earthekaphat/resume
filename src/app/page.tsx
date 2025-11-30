8413d8ffbd08dcbe7071a5bd8938ab9bf2bb5306
Earth <earthekaphat@gmail.com>
2025-11-30 16:12:49 +0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/EarthProfile.jpg"
+          alt="Next.js logo"
+          width={200}
+          height={50}
+          priority
+        />
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Ekaphat Seamthong
+          </h1>
+          <p className="max-w-md text-lg leading-10 text-zinc-600 dark:text-zinc-400"> 
+            Experienced Software Development Engineer in Test with over 3+ years of experience, leading testing
+            operations for multiple mobile and web-based products. Managed both automated and manual testing
+            processes across various platforms to ensure seamless user experiences. Developed and executed
+            comprehensive functional testing and regression testing covering functionality, performance, and
+            usability. Utilized a wide range of tools and technologies to support test environments, CI/CD pipelines,
+            and issue troubleshooting. Collaborated closely with cross-functional teams, including developers,
+            product managers, and UX designers, to drive quality assurance and enhance overall product reliability
+            and release efficiency. Also experienced in mobile testing for both Android and iOS applications
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={16}
+            />
+            Deploy Now
+          </a>
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
